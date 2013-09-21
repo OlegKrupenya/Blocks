23680be9063d788acf41a1299e24eacd002f7f31
Oleg Krupenya <taipan90@rambler.ru>
2013-09-21 23:44:30 +0300

    RotationManager has been added

---
+++ src/main/java/com/udev/process/MovementManager.java
@@ -0,0 +1,11 @@
+package com.udev.process;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author taipan
+ *         Date: 21.09.13
+ *         Time: 20:02
+ */
+public class MovementManager {
+}
---
+++ src/main/java/com/udev/process/RotationManager.java
@@ -0,0 +1,27 @@
+package com.udev.process;
+
+import com.udev.domain.Field;
+import com.udev.domain.figures.Figure;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author taipan
+ *         Date: 21.09.13
+ *         Time: 19:57
+ */
+public class RotationManager {
+
+    /**
+     * Rotates the figure.
+     * @param figure The figure to rotate.
+     * @param field  The Field.
+     */
+    public void rotate(Figure figure, Field field) {
+
+    }
+
+    private void rotateStick(Figure figure, Field field) {
+
+    }
+}
