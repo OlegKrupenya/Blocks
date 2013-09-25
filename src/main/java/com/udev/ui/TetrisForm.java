976ddef9ce7f1e91f8e21144e53b883329a0771b
Oleg Krupenya <oleg.krupenya@akvelon.com>
2013-09-25 22:51:39 +0300

    Drawing of vertical lines

--- src/main/java/com/udev/ui/TetrisForm.java
+++ src/main/java/com/udev/ui/TetrisForm.java
@@ -1,5 +1,8 @@
 package com.udev.ui;
 
+import com.udev.domain.Field;
+import com.udev.process.Executor;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -10,10 +13,27 @@ import java.awt.*;
  */
 public class TetrisForm extends JFrame {
 
+    private Executor executor;
+
     public void paint(Graphics g) {
         Graphics2D ga = (Graphics2D) g;
         ga.setPaint(Color.red);
         ga.drawRect(50, 50, 30 * 10, 30 * 20);
+        drawLines(ga);
+    }
+
+
+    private void drawLines(Graphics2D ga) {
+        int x1 = 50;
+        int y1 = 50;
+        int x2 = 50;
+        int y2 = 50 + 30 * 20;
+        for (int i = 0; i <= 10; i++) {
+            ga.setPaint(Color.gray);
+            ga.drawLine(x1, y1, x2, y2);
+            x1 = x1 + 30;
+            x2 = x2 + 30;
+        }
     }
 
     public static void main(String args[]) {
