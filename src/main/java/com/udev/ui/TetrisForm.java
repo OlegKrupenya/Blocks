b5b4f7188fe07e4ebfcb27cc9db761eaa1299649
Oleg Krupenya <oleg.krupenya@akvelon.com>
2013-09-25 23:00:39 +0300

    Painting of the net

--- src/main/java/com/udev/ui/TetrisForm.java
+++ src/main/java/com/udev/ui/TetrisForm.java
@@ -1,6 +1,5 @@
 package com.udev.ui;
 
-import com.udev.domain.Field;
 import com.udev.process.Executor;
 
 import javax.swing.*;
@@ -19,11 +18,25 @@ public class TetrisForm extends JFrame {
         Graphics2D ga = (Graphics2D) g;
         ga.setPaint(Color.red);
         ga.drawRect(50, 50, 30 * 10, 30 * 20);
-        drawLines(ga);
+        drawHorizontalLines(ga);
+        drawVerticalLines(ga);
+    }
+
+    private void drawHorizontalLines(Graphics2D ga) {
+        int x1 = 50;
+        int y1 = 50;
+        int x2 = 50 + 30 * 10;
+        int y2 = 50;
+        for (int i = 0; i <= 20; i++) {
+            ga.setPaint(Color.gray);
+            ga.drawLine(x1, y1, x2, y2);
+            y1 = y1 + 30;
+            y2 = y2 + 30;
+        }
     }
 
 
-    private void drawLines(Graphics2D ga) {
+    private void drawVerticalLines(Graphics2D ga) {
         int x1 = 50;
         int y1 = 50;
         int x2 = 50;
